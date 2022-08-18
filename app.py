6e8791b0a6e81df0d4cbc791e2374e027f234674
User-nicholas <nick_cn@ymail.com>
2022-08-17 20:48:55 -0700

    na

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ COMMAND = "null"
 def index():
     global PATH, USER_GROUP, USER_NAME, ADMIN_EMAIL, AUTHENTICATION, UPDATE_GROUP_OR_USER, \
         GROUP_VERSION, USER_VERSION, GIT_GROUP, GIT_USER, COMMAND, EM_DATA, PS_DATA
-    get_data()
+    # get_data()
     if wifi_check():
         download_variables()
         if user_authentication():
