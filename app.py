5a026843f02b61448c99250f0a85a65d1d617257
User-nicholas <nick_cn@ymail.com>
2022-08-20 18:40:56 -0700

    na

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ COMMAND = "null"
 def index():
     global PATH, USER_GROUP, USER_NAME, ADMIN_EMAIL, AUTHENTICATION, UPDATE_GROUP_OR_USER, \
         GROUP_VERSION, USER_VERSION, GIT_GROUP, GIT_USER, COMMAND, EM_DATA, PS_DATA
-    # hello group update
+    # hxx
     get_data()
     if wifi_check():
         download_variables()
